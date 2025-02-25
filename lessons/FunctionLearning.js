5f539805ebff95bd17dbb762cbbea56e25dc383d
thanhTruc1011 <thanhtruc1011>
2025-02-25 13:03:13 +0700

    Update lessons

---
+++ lessons/FunctionLearning.js
@@ -0,0 +1,6 @@
+// support function
+// service function - utility function
+for (number = 0; number < 5; number++){
+    console.log(number);
+}
+console.log(number);
---
+++ lessons/day_3.js
@@ -0,0 +1,6 @@
+// Declare a function
+// Method 01: [Function Declaration] method \ hoisting
+// Method 02: [Function Expression] method \ NO hoisting
+// && so sánh
+// || hoặc
+
